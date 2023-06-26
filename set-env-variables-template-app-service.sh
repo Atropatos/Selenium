1a062c66c3793fd6af217fc5a763a78597398250
Dawid Dluski <dd657@inlumine.ual.es>
2023-06-26 20:36:37 +0200

    first commit

---
+++ set-env-variables-template-app-service.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+export COSMOS_URI=<put-your-COSMOS-DB-documentEndpoint-URI-here>
+export COSMOS_KEY=<put-your-COSMOS-DB-primaryMasterKey-here>
+export COSMOS_DATABASE=<put-your-COSMOS-DATABASE-name-here>
+export RESOURCEGROUP_NAME=<put-your-resource-group-name-here>
+export WEBAPP_NAME=<put-your-Webapp-name-here>
+export REGION=<put-your-REGION-here>
+export SUBSCRIPTION_ID=<put-your-SUBSCRIPTION_ID-here>
